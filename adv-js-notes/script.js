b97abcfc88ef7ea40748f458b2f260c2afd9ee25
jramos5883 <jramos5883@gmail.com>
2022-10-19 15:39:49 -0700

    Just adding more notes to my collection

--- adv-js-notes/script.js
+++ adv-js-notes/script.js
@@ -426,3 +426,28 @@ function biggestNumberInArray3(arr) {
     return highest;
   }
 console.log(biggestNumberInArray3(array3));
+
+//LEARNING APIs
+//https://api.giphy.com/v1/gifs/translate?api_key=xKkoxzLGlzTGBu6bMztCXmleIcTcslSR&s=rickandmorty
+
+
+//example using .then, can also use .catch to handle errors
+const img = document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=xKkoxzLGlzTGBu6bMztCXmleIcTcslSR&s=acid', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+});
+
+//example using async and await, both methods are used for async code
+const img = document.querySelector('img');
+
+async function getAcidGIF() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=xKkoxzLGlzTGBu6bMztCXmleIcTcslSR&s=acid', {mode: 'cors'});
+    const acidData = await response.json();
+        img.src = acidData.data.images.original.url;
+}
+
+getAcidGIF();
